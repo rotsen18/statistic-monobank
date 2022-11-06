fc495b1dc90b4de7703b44b0d511fa3ef45332c5
Taras Nester <t.nester@disoft.dev>
2022-11-06 15:14:55 +0200

    add caching requests

--- services/request_service.py
+++ services/request_service.py
@@ -1,39 +1,20 @@
-import json
 import os
 
 import requests
 
 import settings
+from services.cache import Cache
 
 
 class MonobankRequest:
-    CACHE_PATH = os.path.join('services', settings.CACHE_FILENAME)
+    cache = Cache(cache_path=os.path.join('services', settings.CACHE_FILENAME))
 
     @classmethod
     def get_request_data(cls, url: str):
         headers = {'X-Token': settings.TOKEN}
-        data = cls._get_from_cache(url)
+        data = cls.cache.get_from_cache(url)
         if data:
-            print('get from cache', data)
             return data
         data = requests.get(url, headers=headers).json()
-        cls._write_to_cache(url, data)
+        cls.cache.write_to_cache(url, data)
         return data
-
-    @classmethod
-    def _write_to_cache(cls, key: str, value: dict):
-        if not os.path.exists(cls.CACHE_PATH):
-            json.dump({key: value}, open(cls.CACHE_PATH, 'w'))
-            return
-        with open(cls.CACHE_PATH, 'r') as outfile:
-            data = json.load(outfile)
-            data[key] = value
-        json.dump(data, open(cls.CACHE_PATH, 'w'))
-
-    @classmethod
-    def _get_from_cache(cls, key: str):
-        if not os.path.exists(cls.CACHE_PATH):
-            return
-        with open(cls.CACHE_PATH, 'r') as cache:
-            data = json.load(cache)
-            return data.get(key)
